b01cf392b4a7bc289b58a2600a9944d7ae5c3578
Prashanth R <rprashanth1211@gmail.com>
2023-03-23 23:54:51 +1100

    SIT725 - P2 - Added additional calculator get method

--- server.js
+++ server.js
@@ -10,15 +10,65 @@ app.get("/",function(req,res){
     res.render('index.html')
 });
 
-function mathematicalCalculations(){
-    var result = parseInt(number1)+parseInt(number2);
-    return result;
+function mathematicalCalculations(choice,number1,number2){
+    if (choice != null){
+        switch(choice)
+        {
+            case 1:
+                var resultCal = parseInt(number1)+parseInt(number2);
+                return resultCal;
+            case 2:
+                var resultCal = parseInt(number1)-parseInt(number2);
+                return resultCal;
+            case 3:
+                var resultCal = parseFloat(number1)*parseFloat(number2);
+                return resultCal;
+            case 4:
+                var resultCal = parseFloat(number1)/parseFloat(number2);
+                return resultCal;
+            default:
+                return null;
+                
+        }
+    }
+    
 }
 app.get("/addtwoNumbers",(req,res) =>{
     var number1 = req.query.number1
     var number2 = req.query.number2
+    var choice = 1;
 
-    var result = parseInt(number1)+parseInt(number2)
+    var result = mathematicalCalculations(choice,number1,number2)
+
+    res.json({statuscode:200,data:result,message:"success"})
+})
+app.get("/subtwoNumbers",(req,res) =>{
+    var number1 = req.query.number1
+    var number2 = req.query.number2
+
+    var choice = 2;
+
+    var result = mathematicalCalculations(choice,number1,number2);
+
+    res.json({statuscode:200,data:result,message:"success"})
+})
+app.get("/multwoNumbers",(req,res) =>{
+    var number1 = req.query.number1
+    var number2 = req.query.number2
+
+    var choice = 3;
+
+    var result = mathematicalCalculations(choice,number1,number2);
+
+    res.json({statuscode:200,data:result,message:"success"})
+})
+app.get("/dividetwoNumbers",(req,res) =>{
+    var number1 = req.query.number1
+    var number2 = req.query.number2
+
+    var choice = 4;
+
+    var result = mathematicalCalculations(choice,number1,number2);
 
     res.json({statuscode:200,data:result,message:"success"})
 })
